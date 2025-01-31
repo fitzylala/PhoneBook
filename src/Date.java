0b920f357bfc4380efd9349499c218a634c797a6
fitzylala <skye.fitzpatrick100@gmail.com>
2025-01-31 10:54:12 +0000

    Draft of Date file object

--- src/Date.java
+++ src/Date.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 enum Month {
     JANUARY,
     FEBRUARY,
@@ -18,10 +20,10 @@ public class Date {
     private byte day;
     private Month month;
     private int year;
+    private byte maxDaysInMonth;
 
     public Date(byte day, Month month, int year){
 
-        byte maxDaysInMonth;
         switch(month) {
             case FEBRUARY:
                 maxDaysInMonth = 29; break;
@@ -31,9 +33,26 @@ public class Date {
                 maxDaysInMonth = 31; break;
         }
 
-
-
-
+        if (day < 1 || day > maxDaysInMonth) {
+            throw new IllegalArgumentException("Invalid day number for the given month.");
+        } else{
+            this.day = day;
+        }
+        this.month = month;
+        if (year<0) {
+            throw new IllegalArgumentException("Year should not be negative.");
+        } else {
+            this.year = year;
+        }
     }
 
+    public byte getDay() { return day; }
+    public Month getMonth() { return month; }
+    public int getYear() { return  year; }
+
+    /*
+     At this stage I don't need to have different setter methods for day month and year,
+     because it's simpler to replace data with new Day(dd, mmmm, yyyy)
+     This feature could be implemented later if needed
+     */
 }
